b6447f16dc0ccf4bc78b8bd5026fee969ec09c4e
Skynet <webstorybegin.gmail.com>
2021-09-06 01:24:50 +0300

    Lesson 13

---
+++ index.js
@@ -0,0 +1,62 @@
+//12. Практика, ч.1. Начинаем создавать приложение =============
+
+/*const numberOfFils = +prompt("Сколько фильмов вы уже посмотрели?");
+const personalMovieDB = {
+  count: numberOfFils,
+  movies: {},
+  actors: {
+    myFavorite: ''
+  },
+  genres: [],
+  privat: false,
+};
+const one = prompt("Один из первых просмотренных фильмов?"),
+      two = prompt("Насколько оцените его?"),
+      three = prompt("Один из последних просмотренных фильмов?"),
+      four = prompt("Насколько оцените его?"),
+      likeActor = prompt('Любимый ваш актер?');
+      
+
+personalMovieDB.movies[one] = two;
+personalMovieDB.movies[three] = four;
+personalMovieDB.actors[0] = likeActor;
+console.log(personalMovieDB);*/
+
+// 13 условия
+
+// 1
+/*if (4 == 9) {
+  console.log("Это правда!");
+} else {
+  console.log("Вранье");
+}
+
+
+const num = 50;
+
+// 2
+if (num < 49) {
+  console.log('Error')
+} else if (num > 100) {
+  console.log('Очень много')
+}  else {
+  console.log('Ok!')
+}
+
+// 3
+num < 49 ? console.log('Error') : num > 100 ? console.log('Очень много') : console.log('Ok')
+
+//4
+switch (num) {
+  case 49:
+    console.log("Неверно!");
+    break;
+  case 50:
+    console.log("Верно");
+    break;
+  default:
+    console.log('Не сегодня');
+    break; 
+}*/
+
+// 14 Циклы
