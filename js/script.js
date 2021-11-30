b5fb2341605041e23b9e20888776b7ae64b056da
Skynet <webstorybegin.gmail.com>
2021-11-30 23:44:20 +0200

    lesson 61

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// ПРОЕКТ
+///////////////////////////////////////////////////////// ПРОЕКТ
 
 let tabs = document.querySelectorAll(".tabheader__item"),
   tabsContent = document.querySelectorAll(".tabcontent"),
@@ -80,7 +80,7 @@ function showModalByScroll() {
   }
 }
 
-window.addEventListener("scroll", showModalByScroll);
+// window.addEventListener("scroll", showModalByScroll);
 
 // Используем классы для карточек
 
@@ -124,57 +124,76 @@ class MenuCard {
   }
 }
 
-new MenuCard(
-  "img/tabs/vegy.jpg",
-  "vegy",
-  'Меню "Фитнес"',
-  'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-  25,
-  ".menu .container",
-  "menu__item"
-).render();
+const getResource = async (url) => {
+  const res = await fetch(url);
 
-new MenuCard(
-  "img/tabs/elite.jpg",
-  "elite",
-  'Меню "Премиум"',
-  "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-  2,
-  ".menu .container",
-  "menu__item"
-).render();
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, status ${res.status}`);
+  }
 
-new MenuCard(
-  "img/tabs/post.jpg",
-  "post",
-  'Меню "Постное"',
-  "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
-  12,
-  ".menu .container",
-  "menu__item"
-).render();
+  return res.json();
+};
+
+axios.get("http://localhost:3000/menu").then((data) =>
+  data.data.forEach(({ img, altimg, title, descr, price }) => {
+    new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+  })
+);
+
+function createCard(data) {
+  data.forEach(({ img, altimg, title, descr, price }) => {
+    const element = document.createElement("div");
+
+    element.classList.add("menu__item");
+
+    element.innerHTML = `
+      <img src=${img} alt=${altimg}>
+      <h3 class="menu__item-subtitle">${title}</h3>
+      <div class="menu__item-descr">${descr}</div>
+      <div class="menu__item-divider"></div>
+      <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${price}</span> грн/день</div>
+      </div>
+    `;
+
+    document.querySelector(".menu .container").append(element);
+  });
+}
 
 //Forms
 
 const forms = document.querySelectorAll("form");
 
 const message = {
-  loading: "img/spinner.svg",
+  spinner: "img/spinner.svg",
   success: "Спасибо, скоро мы с вами свяжемся",
   failure: "Что-то пошло не так...",
 };
 
 forms.forEach((item) => {
-  postData(item);
+  bindPostData(item);
 });
 
-function postData(form) {
+const postData = async (url, data) => {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: data,
+  });
+
+  return await res.json();
+};
+
+function bindPostData(form) {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const statusMessage = document.createElement("img");
-    statusMessage.src = message.loading;
-    statusMessage.style.cssText = `
+    const spinner = document.createElement("img");
+    spinner.src = message.spinner;
+    spinner.style.cssText = `
       display: block;
       margin: 0 auto;
     `;
@@ -182,19 +201,9 @@ function postData(form) {
 
     const formData = new FormData(form);
 
-    const object = {};
-    formData.forEach(function (value, key) {
-      object[key] = value;
-    });
+    const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-    fetch("server1.php", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(object, null, 2),
-    })
-      .then((data) => data.text())
+    postData("http://localhost:3000/requests", json)
       .then((data) => {
         console.log(data);
         showThanksModal(message.success);
@@ -235,6 +244,64 @@ function showThanksModal(message) {
     closeModal();
   }, 4000);
 }
+
+
+
+// Slider
+
+const slides = document.querySelectorAll(".offer__slide");
+const prev = document.querySelector(".offer__slider-prev");
+const next = document.querySelector(".offer__slider-next");
+const current = document.querySelector('#current')
+const total = document.querySelector('#total')
+
+let indexSlide = 1;
+
+showSlides(indexSlide);
+
+if (slides.length < 10) {
+  total.textContent = `0${slides.length}`;
+} else {
+  total.textContent = slides.length;
+}
+
+function showSlides(n) {
+  if (n > slides.length) {
+    indexSlide = 1
+  }
+
+  if (n < 1) {
+    indexSlide = slides.length;
+  }
+
+  if (slides.length < 10) {
+    current.textContent = `0${indexSlide}`;
+  } else {
+    current.textContent = indexSlide;
+  }
+
+  slides.forEach(item => item.classList.add('hide'));
+  slides[indexSlide - 1].classList.remove('hide');
+}
+
+function plusSlides(n) {
+  showSlides(indexSlide += n);
+}
+
+prev.addEventListener('click', () => {
+  plusSlides(-1)
+})
+
+next.addEventListener("click", () => {
+  plusSlides(+1);
+});
+
+
+
+console.log(currentSlide)
+
+
+
 
 ///////////////////////////////////////////// ТЕОРИЯ
 
@@ -511,7 +578,6 @@ function showThanksModal(message) {
 
 // const result = arr.reduce((sum, current) => `${sum},  ${current}`)
 // console.log(result)
-
 
 // ENTRIES
 
